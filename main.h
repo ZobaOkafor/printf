101000a5cb1facef9fc1fd8f1c80fed78490c246
ZobaOkafor <judithokafor30@gmail.com>
2023-10-15 12:35:35 -0700

    Project Header File

--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <string.h>
+#include <unist.h>
 
+int_putchar(char c);
 int _printf(const char *format, ...);
+int print_integer(int num);
 
 #endif
