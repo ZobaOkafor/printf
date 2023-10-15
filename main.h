0ebf578bbc13fb72052da45c60e4805e1692892a
ZobaOkafor <judithokafor30@gmail.com>
2023-10-15 13:06:25 -0700

    Project header file

--- main.h
+++ main.h
@@ -2,10 +2,9 @@
 #define MAIN_H
 #include <stdarg.h>
 #include <string.h>
-#include <unist.h>
 
-int_putchar(char c);
+int _putchar(char c);
 int _printf(const char *format, ...);
-int print_integer(int num);
+int print_integers(int num);
 
 #endif
